5132d25cec33752e22e03f3aa88ad3ce948a7ca5
Pritesh <pritesh@phatforge.com>
2012-05-28 09:29:11 +0100

    Update for perms

--- lib/generators/templates/assignment.rb
+++ lib/generators/templates/assignment.rb
@@ -1,4 +1,5 @@
 class <%= "Assignment" %> < ActiveRecord::Base
   belongs_to :<%= role_model.tableize.singularize %>
   belongs_to :<%= user_model.tableize.singularize %>
+  validates_uniqueness_of :<%= role_model.tableize.singularize %>_id, scope: :<%= user_model.tableize.singularize %>_id
 end
